d304efd8d303fad8caa27a02cf27d39a4532ac63
Stelios Karabasakis <skarabasakis@gmail.com>
2024-09-07 20:49:21 +0300

    Add zsh-autosuggestions

---
+++ .zshconfig/.zshrc/zsh-autosuggestions.zsh
@@ -0,0 +1,4 @@
+case $(uname) in
+  Darwin) [[ -f /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]] && source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh;;
+  Linux)  [[ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]] && source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh;;
+esac
